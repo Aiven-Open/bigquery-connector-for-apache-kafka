458d3e8960b405edc2448f2dcc5c55bdc0d24f38
Claude Warren <claude.warren@aiven.io>
2025-09-04 09:54:19 +0100

    added deprecation processing and documentation

---
+++ kcbq-connector/src/main/java/io/aiven/kafka/utils/ConfigKeyBuilder.java
@@ -0,0 +1,125 @@
+package io.aiven.kafka.utils;
+
+import static org.apache.kafka.common.config.ConfigDef.NO_DEFAULT_VALUE;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import org.apache.kafka.common.config.ConfigDef;
+
+public class ConfigKeyBuilder<T extends ConfigKeyBuilder<?>> {
+  protected final String name;
+  protected ConfigDef.Type type;
+  protected Object defaultValue;
+  protected ConfigDef.Validator validator;
+  protected ConfigDef.Importance importance;
+  protected String documentation;
+  protected String group;
+  protected int orderInGroup;
+  protected ConfigDef.Width width;
+  protected String displayName;
+  protected Set<String> dependents;
+  protected ConfigDef.Recommender recommender;
+  protected boolean internalConfig;
+
+
+  public ConfigKeyBuilder(String name) {
+    this.name = name;
+    this.type = ConfigDef.Type.STRING;
+    this.displayName = name;
+    this.defaultValue = NO_DEFAULT_VALUE;
+    this.orderInGroup = -1;
+    this.width = ConfigDef.Width.NONE;
+  }
+
+  public ConfigDef.ConfigKey build() {
+    return new ConfigDef.ConfigKey(name, type, defaultValue, validator, importance, documentation, group,
+        orderInGroup, width, displayName, getDependents(), recommender, internalConfig);
+  }
+
+  @SuppressWarnings("unchecked")
+  protected T self() {
+    return (T) this;
+  }
+
+  public final T type(final ConfigDef.Type type) {
+    this.type = type;
+    return self();
+  }
+
+  public final T defaultValue(final Object defaultValue) {
+    this.defaultValue = defaultValue;
+    return self();
+  }
+
+  public final T validator(final ConfigDef.Validator validator) {
+    this.validator = validator;
+    return self();
+  }
+
+  public final T importance(final ConfigDef.Importance importance) {
+    this.importance = importance;
+    return self();
+  }
+
+  public final T documentation(final String documentation) {
+    this.documentation = documentation;
+    return self();
+  }
+
+  public final T group(final String group) {
+    this.group = group;
+    return self();
+  }
+
+  public final T orderInGroup(final int orderInGroup) {
+    this.orderInGroup = orderInGroup;
+    return self();
+  }
+
+  public final T width(final ConfigDef.Width width) {
+    this.width = width;
+    return self();
+  }
+
+  public final T displayName(final String displayName) {
+    this.displayName = displayName;
+    return self();
+  }
+
+  public final T dependents(final Collection<String> dependents) {
+    if (this.dependents == null) {
+      this.dependents = new LinkedHashSet<>(dependents);
+    } else {
+      this.dependents.addAll(dependents);
+    }
+    return self();
+  }
+
+  public final T dependent(final String dependent) {
+    if (this.dependents == null) {
+      this.dependents = new LinkedHashSet<>();
+    }
+    this.dependents.add(dependent);
+    return self();
+  }
+
+  List<String> getDependents() {
+    return dependents == null ? Collections.emptyList() : new LinkedList<>(dependents);
+  }
+
+  public final T recommender(final ConfigDef.Recommender recommender) {
+    this.recommender = recommender;
+    return self();
+  }
+
+  public final T internalConfig(final boolean internalConfig) {
+    this.internalConfig = internalConfig;
+    return self();
+  }
+}
+
+
---
+++ tools/src/test/java/io/aiven/kafka/tools/VelocityDataTests.java
@@ -0,0 +1,42 @@
+package io.aiven.kafka.tools;
+
+
+import io.aiven.kafka.utils.ConfigKeyBuilder;
+import io.aiven.kafka.utils.ExtendedConfigKey;
+import org.apache.kafka.common.config.ConfigDef;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class VelocityDataTests {
+
+    @Test
+    void testExtendedConfigKey() {
+        ExtendedConfigKey extendedConfigKey = ExtendedConfigKey.builder("testOpt").deprecatedInfo(ExtendedConfigKey.DeprecatedInfo.builder()).build();
+        VelocityData underTest = new VelocityData(extendedConfigKey);
+        assertTrue(underTest.isExtendedFlag());
+        assertNotNull(underTest.deprecated());
+        assertNull(underTest.since());
+
+        extendedConfigKey = ExtendedConfigKey.builder("testOpt").deprecatedInfo(ExtendedConfigKey.DeprecatedInfo.builder()).since("Then").build();
+        underTest = new VelocityData(extendedConfigKey);
+        assertTrue(underTest.isExtendedFlag());
+        assertNotNull(underTest.deprecated());
+        assertEquals("Then", underTest.since());
+
+    }
+
+    @Test
+    void testConfigKey() {
+        ConfigDef.ConfigKey configKey = new ConfigKeyBuilder<>("testOpt").build();
+        VelocityData underTest = new VelocityData(configKey);
+        assertFalse(underTest.isExtendedFlag());
+        assertNull(underTest.deprecated());
+        assertNull(underTest.since());
+    }
+
+}
