d3a0a46fd1cb0d9073b7023ddd4b8d82ae94fe3f
Veli Can Ünal <123064963+veliuenal@users.noreply.github.com>
2026-05-01 09:37:46 +0200

    Add putAttemptId to kafka metadata struct for put()-level retry deduplication (#189)

--- kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/BigQuerySinkTask.java
+++ kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/BigQuerySinkTask.java
@@ -59,6 +59,7 @@ import com.wepay.kafka.connect.bigquery.write.storage.StorageWriteApiBase;
 import com.wepay.kafka.connect.bigquery.write.storage.StorageWriteApiBatchApplicationStream;
 import com.wepay.kafka.connect.bigquery.write.storage.StorageWriteApiDefaultStream;
 import com.wepay.kafka.connect.bigquery.write.storage.StorageWriteApiWriter;
+import de.huxhorn.sulky.ulid.ULID;
 import io.aiven.commons.system.VersionInfo;
 import java.time.Instant;
 import java.util.Arrays;
@@ -69,7 +70,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -98,7 +98,8 @@ public class BigQuerySinkTask extends SinkTask {
   private final BigQuery testBigQuery;
   private final Storage testGcs;
   private final SchemaManager testSchemaManager;
-  private final UUID uuid = UUID.randomUUID();
+  private static final ULID ULID_GENERATOR = new ULID();
+  private final String uuid = ULID_GENERATOR.nextULID();
   private final StorageWriteApiBase testStorageWriteApi;
   private final StorageApiBatchModeHandler testStorageApiBatchHandler;
   private final Time time;
@@ -120,6 +121,7 @@ public class BigQuerySinkTask extends SinkTask {
   private KcbqThreadPoolExecutor executor;
   private int remainingRetries;
   private boolean enableRetries;
+  private boolean trackPutAttempts;
   private ErrantRecordHandler errantRecordHandler;
   private boolean useStorageApi;
   private boolean useStorageApiBatchMode;
@@ -281,6 +283,9 @@ public class BigQuerySinkTask extends SinkTask {
 
   @Override
   public void put(Collection<SinkRecord> records) {
+    if (trackPutAttempts) {
+      recordConverter.setCurrentPutAttemptId(ULID_GENERATOR.nextULID());
+    }
     try {
       writeSinkRecords(records);
       remainingRetries = config.getInt(BigQuerySinkConfig.MAX_RETRIES_CONFIG);
@@ -492,6 +497,7 @@ public class BigQuerySinkTask extends SinkTask {
     recordConverter = getConverter(config);
     remainingRetries = config.getInt(BigQuerySinkConfig.MAX_RETRIES_CONFIG);
     enableRetries = config.getBoolean(BigQuerySinkConfig.ENABLE_RETRIES_CONFIG);
+    trackPutAttempts = config.getBoolean(BigQuerySinkConfig.TRACK_PUT_ATTEMPTS_CONFIG);
   }
 
   private void initializeStorageApiMode() {
